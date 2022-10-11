3311f4e0a1a0227f3978ff86105a247d121c1e17
P1xt <P1xt@users.noreply.github.com>
2022-10-11 13:09:37 -0500

    format js files properly

--- app-academy-open/software-engineering/01-web-development-fundamentals/control-flow.js
+++ app-academy-open/software-engineering/01-web-development-fundamentals/control-flow.js
@@ -2,14 +2,14 @@
   Define a function isFive that will return true if a
   number is equal to 5 and false if it is not.
 */
-const isFive = num => num === 5;
+const isFive = (num) => num === 5;
 
 /*
   Write a function isOdd that takes in a number
   as an argument and returns true if the number
   is odd and returns false otherwise.
 */
-const isOdd = num => num % 2 !== 0;
+const isOdd = (num) => num % 2 !== 0;
 
 /*
   So far you've learned how to write your own loops
@@ -23,7 +23,7 @@ const isOdd = num => num % 2 !== 0;
   and false if otherwise.
 */
 const isSubstring = (haystack, needle) =>
-    haystack.toLowerCase().indexOf(needle.toLowerCase()) >= 0;
+  haystack.toLowerCase().indexOf(needle.toLowerCase()) >= 0;
 
 /*
   Define a function logBetween(lowNum, highNum) that will
@@ -34,20 +34,20 @@ const isSubstring = (haystack, needle) =>
   it does not need to return.
 */
 const logBetween = (low, high) => {
-    for (let i = low; i <= high; i++) {
-        console.log(i);
-    }
-}
+  for (let i = low; i <= high; i++) {
+    console.log(i);
+  }
+};
 
 /*
   Write a function printFives(max) that prints out the
   multiples of 5 that are less than the max.
 */
-const printFives = max => {
-    for (let i = 0; i < max; i += 5) {
-        console.log(i);
-    }
-}
+const printFives = (max) => {
+  for (let i = 0; i < max; i += 5) {
+    console.log(i);
+  }
+};
 
 /*
   Write a function logBetweenStepper(min, max, step) that
@@ -62,20 +62,20 @@ const logBetweenStepper = (min, max, step) => {
   for (let i = min; i <= max; i += step) {
     console.log(i);
   }
-}
+};
 
 /*
   Define a function fizzBuzz(max) that takes a
   number and prints every number from 0 to max
   (not inclusive) that is divisible by either 3 or 5, but not both.
 */
-const fizzBuzz = max => {
+const fizzBuzz = (max) => {
   for (let i = 0; i < max; i++) {
     if ((i % 3 === 0 || i % 5 === 0) && !(i % 15 === 0)) {
       console.log(i);
     }
   }
-}
+};
 
 /*
   Write a function combineArray(array1, array2) that takes
@@ -93,7 +93,7 @@ const combineArray = (first, second) => first.concat(second);
   numbers and returns a new array where every element of
   the original array is multiplied by 2.
 */
-const doubler = input => input.map(next => next * 2);
+const doubler = (input) => input.map((next) => next * 2);
 
 /*
   Write a function, countVowels(word), that takes in a string
@@ -101,4 +101,4 @@ const doubler = input => input.map(next => next * 2);
 
   Vowels are the letters "a", "e", "i", "o", "u".
 */
-const countVowels = word => word.match(/[aeiou]/gi).length;
+const countVowels = (word) => word.match(/[aeiou]/gi).length;
